c964f7935e6be908f926b6e6de1fb34386376791
TakahiroMashiko <melto393122@gmail.com>
2021-04-18 04:02:58 +0000

    [Update & Fix] 4-10 application.html.erb,application_controller.rb,users_controller.rbを更新・修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-    # @user = User.find(params[:id])
-    # @book = @user.post_images.page(params[:page]).reverse_order
+    @user = User.find(params[:id])
+    @book = @user.post_images.page(params[:page]).reverse_order
   end
 
   def index
