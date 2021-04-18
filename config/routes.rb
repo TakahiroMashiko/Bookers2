4b2b73aab5bfe46b680f0f29f9901029afe8a633
TakahiroMashiko <melto393122@gmail.com>
2021-04-18 03:41:21 +0000

    [Update & Fix] 4-10 /bookers2/app/views/layouts/application.html.erb,routes.rbを更新・修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ## 使わなかったら削除する
   # root 'books#index'
   # resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :homes, only: [:top]
   resources :books
   resources :users, only: [:show, :index, :edit, :update]
   # アプリケーションのトップ画面を「/」で表示させる
