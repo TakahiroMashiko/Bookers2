6e176f813059569752dce4fe69aae536789f2ac1
TakahiroMashiko <melto393122@gmail.com>
2021-04-18 05:17:45 +0000

    [Update] 4-10 routes.rbを更新・修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/about' => 'homes#about'
-  resources :homes, only: [:top]
+  # get 'home/about' => 'homes#about'
   resources :users, only: [:show, :index, :edit, :update]
   resources :books
   # アプリケーションのトップ画面を「/」で表示させる
