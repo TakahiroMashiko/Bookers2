11b606817b74f191a8f0a5eb2ee33f8bfdce833e
TakahiroMashiko <melto393122@gmail.com>
2021-04-20 13:11:12 +0000

    [Update & Fix] 4-10 bookのindexページの左側の作成(User info)-books_controller.rb,books/index.html.erb,show.html.erbを更新・修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,24 +4,49 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = book.new(book_params)
-    book.save
-    redirect_to user_path(resource)
+    @book = book.new(book_params)
+    @book.save
+    # 投稿処理の成功後、サクセスメッセージを表示
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books=Book.all
+      render 'index'
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    # 更新処理の成功後、サクセスメッセージを表示
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      @books=Book.all
+      render 'edit'
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    # 削除処理の成功後、サクセスメッセージを表示
+    if @book.destroy
+      redirect_to books_path(@book), notice: "Book was successfully destroyed."
+    end
   end
 
   private
