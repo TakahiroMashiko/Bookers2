9e09279db2461560296c8b567eabeb2d5a7e2894
TakahiroMashiko <melto393122@gmail.com>
2021-04-21 15:04:19 +0000

    [Update & Fix] 4-10 bookのindexージの左側の作成(User info)-users_controller.rb,users/edit.html.erbの更新・修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to books_path(@user.id)
+    redirect_to user_path(current_user.id)
   end
 
 # ストロングパラメータを設定
