aa17d87e4fde4667307ba99a8e0ef9b5f3ae9843
TakahiroMashiko <melto393122@gmail.com>
2021-04-22 10:14:15 +0000

    [Finsih & Update & Fix] 4-10 bookのcreate機能の作成-book.rbの更新・修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
   # BookモデルにUserモデルを関連付ける(アソシエーション)
   belongs_to :user, optional: true
 
+  # バリデーションの実装-新規投稿が失敗したとき
+  validates :title, presence: true
+  validates :body, presence: true
+
 end
