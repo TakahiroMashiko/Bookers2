fef8790bf6b5633e8c646ae6c0d8bf3d878d3daa
TakahiroMashiko <melto393122@gmail.com>
2021-04-22 14:04:14 +0000

    [Update & Fix] 4-10 bookのindexページの左側の作成(User info)-users_controller.rb,application_controller_renderer.rbの更新・修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# 以下を追記
+Refile.secret_key = '5573e4e9d992c57571a56b738aa88bbbaf3160fa77fd676dfc1697ed645f53b08b7795f4d48a66d9b80dcb611b2a34cd66c9cb3a9d18b49beb01f6dc9d4ccace'
